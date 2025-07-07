09e55c1fd4966a335d4f0745ce62dd66f14dc157
Alejor0205 <dlejo.r02p@gmail.com>
2025-07-07 18:37:36 -0500

    feat: :sparkles: Se crea variable archivoJson con la funcion de leerJson

--- recomendacion.py
+++ recomendacion.py
@@ -1,1 +1,17 @@
+import json
+
+# Este el la direccion del archivo json donde estan los diccionarios
+archivoJson = 'informacionCantantes.json'
+
+#Funcion para leer el json 
+def leerJson():
+    try:
+        with open(archivoJson,'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("El archivo no se encuentra")
+        return
+    except json.JSONDecodeError:
+        print("Error al leer json")
+        return
 
