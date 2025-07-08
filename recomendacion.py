6dd4e0cf613dc41f808af545dbf3809839aabef2
Alejor0205 <dlejo.r02p@gmail.com>
2025-07-07 22:22:50 -0500

    perf: :zap: Mejorar funcion de "mostraXGeneros"

--- recomendacion.py
+++ recomendacion.py
@@ -17,39 +17,45 @@ def leerJson():
 #Funcion para recomendacion de cantantes segun el genero y tambien sus mejores canciones si asi lo desea el usuario
 def mostrarXGeneros(datos):
     generos= datos['generos']
+    
     print("Los generos disponibles disponibles:\n")
     for i, genero in enumerate(generos,1):
-        print(f"{i}. {genero}")
-    try: 
-        buscadorGenero = input("Seleciona el genero por nombre: \n").strip()
-        if buscadorGenero not in generos:
-            raise ValueError("Genero no existe,ingresa de nuevo el genero")
-        
-        cantantes = generos[buscadorGenero]
-        print(f"\n Cantates recomendados en {buscadorGenero}")
-        for i, singer in enumerate(cantantes, 1):
-            print(f"{i}. {singer['nombre']}")
+            print(f"{i}. {genero}")
+    
+    while True: 
+        try:
+            buscadorGenero = input("Seleciona el genero por nombre: \n").strip()
+            if buscadorGenero not in generos:
+                raise ValueError("Genero no existe,ingresa de nuevo el genero")
+            break     
+        except (ValueError, Exception) as a:
+            print(f"Upss, salio un error inesperado: {a}")
+            
+    cantantes = generos[buscadorGenero]
+    print(f"\n Cantates recomendados en {buscadorGenero}")
+    for i, singer in enumerate(cantantes, 1):
+     print(f"{i}. {singer['nombre']}")
+
+    while True:
         verCanciones = input("\n¿Quieres ver las mejores canciones de alguno? (si/no):\n ").strip().lower()
         if verCanciones == "si":
-            nombre = input("Escribe el nombre del cantante exactamente como aparece: ").strip()
-            cantanteEncontrado = next((a for a in cantantes if a["nombre"].lower() == nombre.lower()), None)
-
-            if cantanteEncontrado:
-                print(f"\n Mejores canciones de {cantanteEncontrado['nombre']}:")
-                for cancion in cantanteEncontrado["canciones"]:
-                    print(f"- {cancion}")
-            else:
-                print("Cantante no encontrado. Asegurate de escribirlo bien")
+            while True:
+                nombre = input("Escribe el nombre del cantante exactamente como aparece: ").strip()
+                cantanteEncontrado = next((a for a in cantantes if a["nombre"].lower() == nombre.lower()), None)
+                if cantanteEncontrado:
+                    print(f"\n Mejores canciones de {cantanteEncontrado['nombre']}:")
+                    for cancion in cantanteEncontrado["canciones"]:
+                     print(f"- {cancion}")
+                    break
+                else:
+                    print("Cantante no encontrado. Asegurate de escribirlo bien")
+                    break
         elif verCanciones == "no":
             print(" Entendido. No se mostraran canciones.")
+            break
         else:
             print("Respuesta no reconocida. Por favor escribe 'si' o 'no'.")
-    except ValueError as a:
-        print(a)
-    except IndexError as e:
-        print(e)
-    except Exception as ae:
-        print(f"Upss, salio un error inesperado: {ae}")
+        
         
 #Funcion mostrar cantantes top globales mediante una lista        
 def mostrarTopGlobal(datos):
